960796508af7572985ff441d6d18b73814fed1b0
Jorgelu09 <158686457+Jorgelu09@users.noreply.github.com>
2024-06-16 15:24:07 -0300

    Nova atualização

--- infinity/Banco_POO.py
+++ infinity/Banco_POO.py
@@ -6,7 +6,8 @@ class Conta:
         self.saldo_conta=0
 
     def cadastro_deconta(self):
-
+        
+        
         self.n_conta=int(input("Digite o número da conta: "))
         self.titular=str(input("Digite o nome do titular: "))
         self.t_conta=str(input("Digite o tipo de conta: (Corrente/Poupança"))
@@ -61,8 +62,19 @@ class Conta_Corrente(Conta):
                 print(f"Saque realizado!!")
                 print(f"Saldo:{self.saldo_conta}|!")
                 
-b1=Conta(n_conta=555368,t_conta="Corrente",titular="Jorge Luis Ferreira da Silva Junior")
-b1.resumo
+class Conta_Poupanca(Conta):
+    
+    def __init__(self, n_conta, titular,taxa_juros):
+        self.taxa_juros=taxa_juros
+                
+        super().__init__(n_conta, "Poupança", titular)
+                
+    def calcular_juros(self):
+        
+        juros=self.saldo_conta*(self.taxa_juros/100)
+        self.saldo_conta+=juros
+        print(F"A taxa do juros é de{juros}")
+        print(F"Saldo:{self.saldo_conta}")
                 
                 
             
